6ea242de0e926c28f58c78e7c9b2ed356a02ec76
임채연 <dlacodus0407@gmail.com>
2025-07-27 00:22:47 +0900

    Feat : docker이름에 맞는 도메인으로 수정한다

--- src/main/java/com/shinhan/pda_midterm_project/common/config/WebClientConfig.java
+++ src/main/java/com/shinhan/pda_midterm_project/common/config/WebClientConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    private static final String DATA_SERVER_BASE_URL = "http://127.0.0.1:8000";
+    private static final String DATA_SERVER_BASE_URL = "http://fastapi:8000";
 
     @Bean
     public WebClient webClient() {
